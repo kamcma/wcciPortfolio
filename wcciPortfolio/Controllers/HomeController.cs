b093ea4e7db565723691c1e7241d206c2c8a80c4
Kyle McMahon <kyle.mcmahon@mac.com>
2016-05-24 14:37:59 -0400

    Database model hooked up, implemented in index view

--- wcciPortfolio/Controllers/HomeController.cs
+++ wcciPortfolio/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wcciPortfolio.Models;
 
 namespace wcciPortfolio.Controllers
 {
     public class HomeController : Controller
     {
+        private wcciPortfolioProjectsEntities db = new wcciPortfolioProjectsEntities();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Projects.ToList());
         }
 
         public ActionResult About()
