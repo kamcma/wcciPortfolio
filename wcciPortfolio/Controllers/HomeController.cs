9cd1ebdec8b9038a988b1d9e00c033bd848e12d4
Kyle McMahon <kyle.mcmahon@mac.com>
2016-05-25 15:59:24 -0400

    Edits to index page

--- wcciPortfolio/Controllers/HomeController.cs
+++ wcciPortfolio/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace wcciPortfolio.Controllers
 
         public ActionResult Index()
         {
-            return View(db.Projects.ToList());
+            return View(ShuffleList(db.Projects.ToList()));
         }
 
         public ActionResult About()
@@ -29,5 +29,22 @@ namespace wcciPortfolio.Controllers
 
             return View();
         }
+
+        private List<T> ShuffleList<T>(List<T> inputList)
+        {
+            List<T> randomList = new List<T>();
+
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+                randomList.Add(inputList[randomIndex]); //add it to the new, random list
+                inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+            }
+
+            return randomList; //return the new random list
+        }
+
     }
 }
